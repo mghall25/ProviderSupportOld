403662b8a75103f857ba39fa282c2a577dce62fc
MGH <miriamhall.mgh@gmail.com>
2018-05-02 10:53:17 -0700

    With CRUD working
    Directly after first tutorial

--- ProviderSupport/App_Start/BundleConfig.cs
+++ ProviderSupport/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace ProviderSupport
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/Litera.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
